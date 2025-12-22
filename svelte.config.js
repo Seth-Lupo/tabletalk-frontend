1d0b2a58f34779c1830d81a5637e47c183bb4c4b
Seth-Lupo <seth@squibber.com>
2025-12-22 01:14:00 -0500

    giuthub actions

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
